2cdb606267ca79a908d990cf458d3d313818e676
Reece McGowan <reecem@sfu.ca>
2020-07-08 13:08:58 -0700

    Moved violation tests from InspectionTest to ViolationTest
    Since Violation has its own class, created a separate test class.
    Removed InspectionTest since there are no tests for the class.

--- app/src/test/java/com/example/group20restaurantapp/Model/ViolationTest.java
+++ app/src/test/java/com/example/group20restaurantapp/Model/ViolationTest.java
@@ -6,59 +6,71 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class InspectionTest {
+public class ViolationTest {
 
     @Test
     public void testViolImgId1() {
         // Test standalone violation
-        Inspection.Violation viol = new Inspection.Violation("Follow-up",
+        Violation viol = new Violation(101,
+                false,
                 "???",
-                101);
+                "???",
+                false);
         assertEquals(R.drawable.violation_construction, viol.getViolImgId());
     }
 
     @Test
     public void testViolImgId2() {
         // Test or statement
-        Inspection.Violation viol = new Inspection.Violation("Follow-up",
+        Violation viol = new Violation(305,
+                false,
                 "???",
-                305);
+                "???",
+                false);
         assertEquals(R.drawable.violation_rat, viol.getViolImgId());
     }
 
     @Test
     public void testViolImgId3() {
         // Test no matching violation number
-        Inspection.Violation viol = new Inspection.Violation("Follow-up",
+        Violation viol = new Violation(99,
+                false,
                 "???",
-                99);
+                "???",
+                false);
         assertEquals(R.drawable.violation_generic, viol.getViolImgId());
     }
 
     @Test
     public void testViolImgId4() {
         // Test permitArrayList
-        Inspection.Violation viol = new Inspection.Violation("Follow-up",
+        Violation viol = new Violation(314,
+                false,
                 "???",
-                314);
+                "???",
+                false);
         assertEquals(R.drawable.violation_permit, viol.getViolImgId());
     }
 
     @Test
     public void testViolImgId5() {
         // Test badFoodArrayList
-        Inspection.Violation viol = new Inspection.Violation("Follow-up",
+        Violation viol = new Violation(206,
+                false,
                 "???",
-                206);
+                "???",
+                false);
         assertEquals(R.drawable.violation_bad_food, viol.getViolImgId());
     }
 
     @Test
     public void testViolImgId6() {
         // Test utensilsArrayList
-        Inspection.Violation viol = new Inspection.Violation("Follow-up",
+        Violation viol = new Violation(308,
+                false,
                 "???",
-                308);
+                "???",
+                false);
         assertEquals(R.drawable.violation_utensils, viol.getViolImgId());
     }
 
