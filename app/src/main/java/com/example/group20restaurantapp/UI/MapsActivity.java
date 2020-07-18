90af2a1e9facbd3f57efab203849897ca1c19269
Reece McGowan <reecem@sfu.ca>
2020-07-17 22:50:17 -0700

    Imported toast

--- app/src/main/java/com/example/group20restaurantapp/UI/MapsActivity.java
+++ app/src/main/java/com/example/group20restaurantapp/UI/MapsActivity.java
@@ -9,6 +9,7 @@ import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
